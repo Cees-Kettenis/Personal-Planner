00e5483e7988010ea9cff0e8af63e69a97b20997
Cees-Kettenis <101450983+Cees-Kettenis@users.noreply.github.com>
2024-06-11 16:36:32 +0800

    added global image with gravatar
    1. moved gravatar code from user_html.ex to personal_planner_utils.ex

--- lib/personal_planner_utils.ex
+++ lib/personal_planner_utils.ex
@@ -6,4 +6,14 @@ defmodule PersonalPlannerWebUtils do
       "Invalid Date"
     end
   end
+
+  defp md5_hexdigest(str) do
+    :crypto.hash(:md5, str)
+    |> Base.encode16(case: :lower)
+  end
+
+  def gravatar_for(user) do
+    gravatar_id = String.downcase(user.email) |> md5_hexdigest()
+    ["https://secure.gravatar.com/avatar/", gravatar_id]
+  end
 end
--- lib/personal_planner_web/controllers/user_html.ex
+++ lib/personal_planner_web/controllers/user_html.ex
@@ -10,14 +10,4 @@ defmodule PersonalPlannerWeb.UserHTML do
   attr :action, :string, required: true
 
   def user_form(assigns)
-
-  defp md5_hexdigest(str) do
-    :crypto.hash(:md5, str)
-    |> Base.encode16(case: :lower)
-  end
-
-  def gravatar_for(user) do
-    gravatar_id = String.downcase(user.email) |> md5_hexdigest()
-    ["https://secure.gravatar.com/avatar/", gravatar_id]
-  end
 end
