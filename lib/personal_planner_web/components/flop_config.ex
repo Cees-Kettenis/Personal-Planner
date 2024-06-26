b18cd07cf5befd88843ceadbadc72df0c9a89935
Cees-Kettenis <101450983+Cees-Kettenis@users.noreply.github.com>
2024-06-26 10:19:32 +0800

    responsive design
    1. made the entire app responsive.

--- lib/personal_planner_web/components/flop_config.ex
+++ lib/personal_planner_web/components/flop_config.ex
@@ -2,7 +2,7 @@ defmodule PersonalPlannerWeb.FlopConfig do
 
   def pagination_opts do
     [
-      page_links: {:ellipsis, 5},
+      page_links: {:ellipsis, 3},
       ellipsis_content: Phoenix.HTML.raw("..."),
       ellipsis_attrs: [
         class: "p-2 mx-1 text-gray-400"
